b442436ec7a47f13ccd4d29dfab195f6024ebac9
prasetya12 <rizalprasetya22@gmail.com>
2025-08-15 20:22:29 +0700

    fix:use dynamic for supported langs and also for import resources from yaml provided

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,21 +1,20 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 // import enTranslation from './assets/locales/en/translations.json'
-import enTranslation from './assets/locales/en/translation.yaml'
-import esTranslation from './assets/locales/es/translation.yaml'
-import itTranslation from './assets/locales/it/translation.yaml'
+// import enTranslation from './assets/locales/en/translation.yaml'
+// import esTranslation from './assets/locales/es/translation.yaml'
+// import itTranslation from './assets/locales/it/translation.yaml'
+import dictionaries from "./assets/locales";
 
-export const supportedLngs = ["en", "es", "it"]
+export const supportedLngs = Object.keys(dictionaries);
+
+// export const supportedLngs = ["en", "es", "it"]
 i18n
   .use(initReactI18next)
   .init({
     lng: "en",
     supportedLngs,
-    resources: {
-      en: { translation: enTranslation },
-      es: { translation: esTranslation },
-      it: { translation: itTranslation }
-    },
+    resources: dictionaries,
     fallbackLng: "en",
     react: { useSuspense: false },
   });
