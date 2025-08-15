4bfada0fbcb907e45c5ffd93a2e3437105b2b7f0
Slackluky <unluckly17@gmail.com>
2025-08-16 03:15:35 +0700

    refactor: remove unnecessary console logs

--- src/components/ui/select-language.tsx
+++ src/components/ui/select-language.tsx
@@ -18,7 +18,6 @@ export function SelectLanguage() {
 
     // Redirect to the new language path
     newPath = newPath.startsWith('/en/') ? newPath.replace(/^\/en/, '') : newPath
-    console.log({ newPath })
     window.location.href = newPath;
   };
 
