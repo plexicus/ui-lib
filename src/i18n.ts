4f913b9c10864a9459adcd73e2f61e6f11dcc2a8
Slackluky <unluckly17@gmail.com>
2025-08-21 20:03:57 +0700

    chore: remove unused i18n imports

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,9 +1,5 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-// import enTranslation from './assets/locales/en/translations.json'
-// import enTranslation from './assets/locales/en/translation.yaml'
-// import esTranslation from './assets/locales/es/translation.yaml'
-// import itTranslation from './assets/locales/it/translation.yaml'
 import dictionaries from "./assets/locales";
 
 export const supportedLngs = Object.keys(dictionaries);
