ff6e8471a333e47258f1970682b98afa8b64e5e8
Slackluky <unluckly17@gmail.com>
2025-08-29 19:20:37 +0700

    refactor(gtm): replace GTM hook with analytics export
    This commit replaces the `GoogleAnalytics` component and
    `useTrackClick` hook with a direct `analytics` export from the
    `lib/GTM` module. This simplifies the integration and provides a
    more streamlined approach to using Google Tag Manager.

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,4 @@ export {NavbarPlexicus} from "./components/Navbar"
 export {FooterPlexicus} from "./components/Footer"
 export { SelectLanguage } from "./components/ui/select-language"
 export { CommandDialogSearch } from "./components/command-input";
-export {GoogleAnalytics} from './components/GTM'
-export {useTrackClick} from './hooks/gtm-event-tracker'
+export {analytics} from './lib/GTM'
