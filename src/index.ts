2fe53ba0db03f1ec32df1c68ca9bfc3aa4e5828f
Slackluky <unluckly17@gmail.com>
2025-09-01 21:20:13 +0700

    feat(ui): import cookieconsent.css
    This commit imports the cookieconsent.css file to apply the default styles for the cookie consent component. This ensures that the component is displayed correctly and consistently across different browsers and devices.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 
 
 import './index.css'
+import 'vanilla-cookieconsent/dist/cookieconsent.css';
 export {NavbarPlexicus} from "./components/Navbar"
 export {FooterPlexicus} from "./components/Footer"
 export { SelectLanguage } from "./components/ui/select-language"
