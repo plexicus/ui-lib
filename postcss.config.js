7379a039fbef65c5b7037be49231bc8e4554316b
Slackluky <unluckly17@gmail.com>
2025-09-02 13:28:51 +0700

    fix(ui): exclude cookie consent styles from prefixing
    This commit updates the postcss configuration to prevent the
    `src/components/CookieConsent/styles.css` file from being prefixed with
    the `.ui-lib` class. This ensures that the cookie consent component's
    styles are not inadvertently modified by the prefixing process,
    preventing potential styling conflicts or unexpected behavior.

--- postcss.config.js
+++ postcss.config.js
@@ -4,7 +4,8 @@ export default {
       'postcss-prefix-selector': {
             prefix: '.ui-lib', // The class that will scope your styles
             ignoreFiles: [
-                'node_modules/vanilla-cookieconsent/dist/cookieconsent.css'
+                'node_modules/vanilla-cookieconsent/dist/cookieconsent.css',
+                'src/components/CookieConsent/styles.css'
             ],
             // Optional: transform to handle special cases like 'body' or ':root'
             transform: (prefix, selector, prefixedSelector) => {
